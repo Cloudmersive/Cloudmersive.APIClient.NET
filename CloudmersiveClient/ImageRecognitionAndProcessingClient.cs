8d146ce5328e083db7c31459eb54fbe007996323
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2018-01-17 23:02:11 -0800

    Added fn

---
+++ CloudmersiveClient/ImageRecognitionAndProcessingClient.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudmersiveClient
+{
+    public class ImageRecognitionAndProcessingClient : CloudmersiveClientBase
+    {
+        public ImageRecognitionAndProcessingClient(string apikey)
+        {
+            Apikey = apikey;
+        }
+
+        public ImageRecognitionAndProcessingClient()
+        {
+            LoadApikeyFromConfig();
+        }
+
+        public string RecognizeImageToDescription(byte[] imageBytes)
+        {
+            using (WebClient client = new WebClient())
+            {
+                client.Headers.Add("Apikey", Apikey);
+                client.Headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
+
+
+
+                var bytes = imageBytes;
+
+                var response = client.UploadData("https://api.cloudmersive.com/image/recognize/describe", "POST", bytes);
+
+                string result = System.Text.Encoding.ASCII.GetString(response);
+
+                result = Helpers.DecodeEscapedStrings(result);
+
+                return result;
+            }
+        }
+    }
+}
