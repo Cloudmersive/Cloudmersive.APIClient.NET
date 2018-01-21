88d7e81f35945ca1a57a186de08d3ecd8ef7bcb8
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2018-01-20 21:21:18 -0800

    Added

--- CloudmersiveClient/CloudmersiveVirusScanClient.cs
+++ CloudmersiveClient/CloudmersiveVirusScanClient.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace CloudmersiveClient
 {
-    public class CloudmersiveVirusScanClient
+    public class CloudmersiveVirusScanClient : CloudmersiveClientBase
     {
+        public CloudmersiveVirusScanClient(string apikey)
+        {
+            Apikey = apikey;
+        }
+
+        public CloudmersiveVirusScanClient()
+        {
+            LoadApikeyFromConfig();
+        }
     }
 }
