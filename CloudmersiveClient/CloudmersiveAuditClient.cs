7bc353fb96612267dd36ce5f12aa82d25e2d4df7
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2018-01-27 13:57:21 -0800

    Added

--- CloudmersiveClient/CloudmersiveAuditClient.cs
+++ CloudmersiveClient/CloudmersiveAuditClient.cs
@@ -1,12 +1,45 @@
-﻿using System;
+﻿using CloudmersiveClient.Folder;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CloudmersiveClient
 {
-    public class CloudmersiveAuditClient
+    public class CloudmersiveAuditClient : CloudmersiveClientBase
     {
+        public CloudmersiveAuditClient(string apikey)
+        {
+            Apikey = apikey;
+        }
+
+        public CloudmersiveAuditClient()
+        {
+            LoadApikeyFromConfig();
+        }
+
+        public AuditLogResponse WriteLogMessageFull(AuditLogWriteRequest req)
+        {
+            using (WebClient client = new WebClient())
+            {
+                client.Headers.Add("Apikey", Apikey);
+                client.Headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
+
+
+
+                var bytes = System.Text.Encoding.ASCII.GetBytes("=" + JsonConvert.SerializeObject(req)  );
+
+                var response = client.UploadData("https://api.cloudmersive.com/validate/email/address/full", "POST", bytes);
+
+                string result = System.Text.Encoding.ASCII.GetString(response);
+
+
+
+                return JsonConvert.DeserializeObject<AuditLogResponse>(result);
+            }
+        }
     }
 }
