01b32770506b6e20e7acdbf330f41a863e69dfcb
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2018-01-27 13:55:19 -0800

    Added

---
+++ CloudmersiveClient/Folder/Audit.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudmersiveClient.Folder
+{
+    public class AuditLogWriteRequest
+    {
+        public string AuditLogMessage;
+        public string AuditLogMeta;
+        public string AuditLogUserIdentifier;
+        public string AuditLogReferenceID;
+        public string AuditLogReferenceIP;
+        public string AuditLogReferenceLocation;
+        public string AuditLogAction;
+    }
+
+    public class AuditLogResponse
+    {
+        public bool Successful;
+    }
+}
