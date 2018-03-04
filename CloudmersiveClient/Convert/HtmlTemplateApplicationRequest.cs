931f7de5c685c9a4f6afd056a2609420dbc57ad0
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2018-03-04 12:38:37 -0800

    Added

---
+++ CloudmersiveClient/Convert/HtmlTemplateApplicationRequest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudmersiveClient.Convert
+{
+    /// <summary>
+    /// HTML template application request
+    /// </summary>
+    public class HtmlTemplateApplicationRequest
+    {
+        /// <summary>
+        /// HTML template input as a string
+        /// </summary>
+        public string HtmlTemplate;
+
+        /// <summary>
+        /// Operations to apply to this template
+        /// </summary>
+        public HtmlTemplateOperation[] Operations;
+    }
+
+    /// <summary>
+    /// Valid operations that can be applied to an HTML template
+    /// </summary>
+    public enum HtmlTemplateOperationAction
+    {
+        /// <summary>
+        /// Replace a string
+        /// </summary>
+        Replace = 1
+    }
+
+    public class HtmlTemplateOperation
+    {
+        /// <summary>
+        /// Operation action to take
+        /// </summary>
+        public HtmlTemplateOperationAction Action;
+
+        /// <summary>
+        /// For Replace operations, the string to match against (to be replaced with ReplaceWith string)
+        /// </summary>
+        public string MatchAgsint;
+
+        /// <summary>
+        /// For Replace operations, the string to Replace the original string with
+        /// </summary>
+        public string ReplaceWith;
+    }
+}
---
+++ CloudmersiveClient/Convert/HtmlTemplateApplicationResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudmersiveClient.Convert
+{
+    /// <summary>
+    /// Response from an HTML template application
+    /// </summary>
+    public class HtmlTemplateApplicationResponse
+    {
+        /// <summary>
+        /// True if the operation was successful, false otherwise
+        /// </summary>
+        public bool Successful;
+
+        /// <summary>
+        /// Final HTML result of all operations on input
+        /// </summary>
+        public string FinalHtml;
+    }
+}
