bcf02d33a458c580480bb03fa82e616d64ef9a23
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2018-05-11 22:47:42 -0700

    Added classes

---
+++ CloudmersiveClient/Validation/VatLookupRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudmersiveClient.Validation
+{
+    public class VatLookupRequest
+    {
+        /// <summary>
+        /// VAT code to lookup; example "CZ25123891"
+        /// </summary>
+        public string VatCode;
+    }
+}
---
+++ CloudmersiveClient/Validation/VatLookupResponse.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudmersiveClient.Validation
+{
+    public class VatLookupResponse
+    {
+        /// <summary>
+        /// Two-letter country code
+        /// </summary>
+        public string CountryCode;
+
+        /// <summary>
+        /// VAT number
+        /// </summary>
+        public string VatNumber;
+
+        /// <summary>
+        /// True if the VAT code is valid, false otherwise
+        /// </summary>
+        public bool IsValid;
+
+        /// <summary>
+        /// Name of the business
+        /// </summary>
+        public string BusinessName;
+
+        /// <summary>
+        /// Business address
+        /// </summary>
+        public string BusinessAddress;
+    }
+}
